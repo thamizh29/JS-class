f482b699b58de42d089b1b9ed7e54d94916370d1
thamizh29 <93852143+thamizh29@users.noreply.github.com>
2022-06-12 10:21:18 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,13 @@
+//class
+class vehical{
+    constructor(name,model,color)
+    {
+    this.cl_name=name;
+    this.cl_model=model;
+    this.cl_color=color;
+    }
+}
+vehical1=new vehical("Bike","Pulsar-150","Black");
+vehical2=new vehical("Car","BMW","Blue");
+vehical3=new vehical("Tractor","Swaraj-855","Red");
+console.log(vehical1.cl_model);
